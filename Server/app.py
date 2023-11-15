0f0a8904481c824a89fd87a74832e492429eabce
c0ldheart <thecoldheart@foxmail.com>
2023-11-15 15:09:54 +0800

    ✨ feat(app): app.py上传

--- Server/app.py
+++ Server/app.py
@@ -1,19 +1,12 @@
 from flask_cors import *
 from flask import Flask, request, jsonify, send_file
-import os
-import io
-from PIL import Image
-import json
-from src.service.predict import Predictor
+from src.service.Predictor import Predictor
+from src.service.FileHelper import FileHelper
 app = Flask(__name__)
 app.config['MODEL_PATH'] = '/home/belizabeth/zjk/DCI-VTON-Virtual-Try-On'
 CORS(app, supports_credentials=True)
 
-predictor = Predictor()
-print("model initializing...")
-predictor.initialize()
-print("model initialized")
-    
+file_helper = FileHelper(app.config)
     
 @app.route('/')
 def hello_world():
@@ -27,60 +20,25 @@ def upload():
     file = request.files['file']
     if file.filename == '':
         return jsonify({'message': 'No file uploaded errorcode:2'})
-    filename = file.filename
-    image_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    if os.path.exists(image_url):
-        return jsonify({'message': 'File already exists errorcode:3',
-                        'image_url': image_url})
+    file = file.read()
+    message, file_hash, path = file_helper.save(file)
+    print("文件保存：", path)
+    return jsonify({'message': message, 'image_url': path})
+  
+@app.route('/tryon', methods=['POST'])
+def tryon():
+    cloth_id = request.form['clothId']
+    # human_hash = request.form['humanHash']
+    human_image = request.files['humanImage']
+    message, file_hash, human_path = file_helper.save(file=human_image.read())
+    print(message)
+    predictor = Predictor(cloth_id, human_path, file_hash)
+    res = predictor.preprocess()
+    predictor.inference()
 
-    file.save(image_url)
-    print("文件保存：", image_url)
-    return jsonify({'image_url': image_url})
-
-@app.route('/predict', methods=['POST'])
-def predict():
-
-
-    # 获得json
-    data = json.loads(request.data)
-    image1_url = data['image1'][0]['response']['image_url']
-    print("image1 url:", image1_url)
-
-    image2_url = data['image2'][0]['response']['image_url']
-    print("image2 url:", image2_url)
-
-    print("preprocess...")
-    predictor.preprocess(image1_url, image2_url)
-    print("preprocess done")
-    print("inference...")
-    tryon_tensor = predictor.inference()
-    print("inference done")
-    result_path = predictor.postprocess(tryon_tensor)
-
-    return send_file(result_path)
-    # return jsonify({'res_path': result_path})
-@app.route('/predict_test', methods=['POST'])
-def predict_test():
-
-
-    # 获得json
-    # data = json.loads(request.data)
-    image1_url = 'results/000001_0.jpg'
+    return jsonify({'message': res}) 
     
 
-    image2_url = 'results/000001_0.jpg'
-    
-
-    print("preprocess...")
-    predictor.preprocess(image1_url, image2_url)
-    print("preprocess done")
-    print("inference...")
-    tryon_tensor = predictor.inference()
-    print("inference done")
-    result_path = predictor.postprocess(tryon_tensor)
-
-    return send_file(result_path)
 if __name__ == '__main__':
-
-    app.run(debug=True , host='0.0.0.0', port=6000)
+    app.run(debug=False , host='0.0.0.0', port=6000)
     # app.run(debug=True , host='0.0.0.0', port=6000, ssl_context=("/home/ubuntu/VITON01/FlaskServer/.well-known/certificate.crt", "/home/ubuntu/VITON01/FlaskServer/.well-known/private.key"))
---
+++ Server/src/service/Predictor.py
@@ -0,0 +1,171 @@
+import os
+import cv2
+ 
+class Predictor():
+    """
+    A custom model handler implementation.
+    """
+    def __init__(self, cloth_id, human_image_path, name):
+        self.name = name
+        self.cloth_id = cloth_id
+        self.human_image_path = human_image_path
+        self.work_dir = "/home/belizabeth/zjk/DCI-VTON-Virtual-Try-On"
+        self.openpose_dir = "/home/belizabeth/zjk/openpose-master/build/examples/openpose"
+        self.densepose_dir = "/home/belizabeth/zjk/TryYours-Virtual-Try-On"
+        self.humanparsing_dir = "/home/belizabeth/zjk/TryYours-Virtual-Try-On/Graphonomy-master"
+        self.clothes_dir = "/home/belizabeth/zjk/VITONHD_dataset/test"
+        self.preprocessed_dir = "/home/belizabeth/zjk/DCI-VTON-Virtual-Try-On/preprocessed_dir"
+        self.warpmodel_dir = "/home/belizabeth/zjk/DCI-VTON-Virtual-Try-On/PF-AFN/PF-AFN_test"
+        self.preprocessed_dir_list = [
+            "image",
+            "openpose_img",
+            "openpose_json",
+            "densepose",
+            "seg",
+            "warped_clothes",
+            "warped_mask",
+        ]   
+        self.preprocessed_dir_dict = {}
+        
+    def preprocess(self):
+        print('start preprocess')
+        try:
+            self.init_preprocess()
+            self.openpose()
+            self.densepose()
+            self.human_parse()
+            self.warp_cloth(self.cloth_id)
+        except Exception as e:
+            print(e)
+            return "preprocess fail"
+        return "preprocess success!"
+    def inference(self):
+        """
+        Internal inference methods
+        :param model_input: transformed model input data
+        :return: list of inference output in NDArray
+        """
+        res_dir = os.path.join(self.work_dir, "results", self.name)
+        os.makedirs(res_dir, exist_ok=True)
+        os.chdir(self.work_dir)
+        terminnal_command = (
+            "python test.py --plms --gpu_id 0 \
+            --ddim_steps 100 \
+            --person_name %s \
+            --outdir %s \
+            --config configs/viton512.yaml \
+            --dataroot %s \
+            --preprocessed_dir %s \
+            --ckpt /home/belizabeth/zjk/DCI-VTON-Virtual-Try-On/checkpoints/viton512.ckpt \
+            --n_samples 1 \
+            --seed 23 \
+            --scale 1 \
+            --H 512 \
+            --W 512 \
+            --unpaired"
+            % (self.name, res_dir, self.clothes_dir, os.path.join(self.preprocessed_dir, self.name))
+        )
+        os.system(terminnal_command)
+        
+    
+    def init_preprocess(self) -> None:
+        os.chdir(path=self.work_dir)
+        self.preprocessed_dir = os.path.abspath(self.preprocessed_dir)
+        os.makedirs(name=self.preprocessed_dir, exist_ok=True)
+        img_src = cv2.imread(self.human_image_path)
+        img_byte = img_src.tobytes()
+        cur_persson_file_name = self.name
+        cur_persson_file_dir = os.path.join(self.preprocessed_dir, cur_persson_file_name)
+
+        self.preprocessed_dir_dict = {}
+        for i in range(0, len(self.preprocessed_dir_list)):
+            path = os.path.join(cur_persson_file_dir, self.preprocessed_dir_list[i])
+            os.makedirs(path, exist_ok=True)
+            self.preprocessed_dir_dict[self.preprocessed_dir_list[i]] = path
+        print("\033[32mMake image at: \033[0m%s" % self.human_image_path)
+
+    def openpose(self):
+        print_line("Get openpose coordinate (rendered image and json file)")
+
+        os.chdir(self.openpose_dir)
+        terminnal_command = (
+            "./openpose.bin --model_pose BODY_25 --write_coco_json_variants 1 \
+                --model_folder /home/belizabeth/zjk/openpose-master/models --display 0 --hand --disable_blending\
+                --image_dir %s \
+                --write_json %s \
+                --write_images %s"
+            % (
+                self.preprocessed_dir_dict["image"],
+                self.preprocessed_dir_dict["openpose_json"],
+                self.preprocessed_dir_dict["openpose_img"],
+            )
+        )
+        os.system(terminnal_command)
+        print_line("")
+    
+    def densepose(self):
+        print_line("Getting densepose at%s" % self.densepose_dir)
+
+        os.chdir(self.densepose_dir)
+        os.environ["MKL_THREADING_LAYER"] = "GNU"
+        # os.system("export MKL_SERVICE_FORCE_INTEL=1")
+        terminnal_command = (
+            "python detectron2/projects/DensePose/apply_net.py \
+            dump \
+            detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml \
+            https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl \
+            %s \
+            --output output.pkl -v"
+            % self.human_image_path
+        )
+        # print(terminnal_command)
+
+        os.system(terminnal_command)
+        terminnal_command = "python get_densepose.py -p %s -s %s" % (
+            self.human_image_path,
+            os.path.join(
+                self.preprocessed_dir_dict["densepose"], os.path.basename(self.name) + '.jpg'
+            ),
+        )
+        os.system(terminnal_command)
+        
+    def human_parse(self):
+        print("\033[32mGetting parsing at%s\033[0m" % self.humanparsing_dir)
+        os.chdir(self.humanparsing_dir)
+        terminnal_command = (
+            "python exp/inference/inference.py \
+            --loadmodel ./inference.pth \
+            --img_path %s --output_path %s --output_name %s"
+            % (
+                self.human_image_path,
+                self.preprocessed_dir_dict["seg"],
+                self.name,
+            )
+        )
+        os.system(terminnal_command)
+
+    def warp_cloth(self, cloth_id):
+        print_line("Warp clothes at%s" % self.warpmodel_dir)
+        os.chdir(self.warpmodel_dir)
+        terminnal_command = (
+            "python -u eval_PBAFN_viton.py --name=%s \
+        --resize_or_crop=none --batchSize=1 --gpu_ids=0 \
+        --warp_checkpoint=/home/belizabeth/zjk/DCI-VTON-Virtual-Try-On/checkpoints/warp_viton.pth \
+        --label_nc=13 --fineSize=512 --unpaired\
+        --dataroot=%s \
+        --preprocessed_dir=%s \
+        --saved_path=%s  \
+        --cloth_id=%s"
+            % (
+                self.name,
+                self.clothes_dir,
+                os.path.join(self.preprocessed_dir,self.name),
+                self.preprocessed_dir_dict["warped_clothes"],
+                str(cloth_id)
+            )
+        )
+        os.system(terminnal_command)
+        
+def print_line(msg):
+    print("\033[32m-------------------------------------------\033[0m")
+    print("\033[32m%s\033[0m" % msg)
